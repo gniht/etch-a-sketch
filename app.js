b8261f69b6f8161c45cd5ef293d23711fdfe04d9
Jason Thomas <jasonthomasjat@gmail.com>
2020-12-16 11:19:46 -0800

    added hover colorchange to cells and adjusted cell size to fit 800px square

--- app.js
+++ app.js
@@ -2,19 +2,24 @@
 
 function squareDivGrid(dimension){
   const gridContainer = document.createElement("div");
-  gridContainer.id = "container";
+  gridContainer.classList.add("container");
+  gridContainer.setAttribute("style",`grid-template-columns: repeat(${dimension}, auto)`);  
   document.querySelector('body').append(gridContainer);
+  const size = 800/dimension;
   for (let i = 0; i < dimension; i++){
     for (let j = 0; j < dimension; j++){
       let temp = document.createElement('div');
-      temp.classList.add(`row${i}`, `column${j}`);
-      //temp.innerText = `${i},${j}`;
+      temp.classList.add(`cell`,`${i}-${j}`);
+      temp.setAttribute("style", `width: ${size}px; height: ${size}px;`);
       gridContainer.append(temp);
     }
   }  
 }
-
-squareDivGrid(16);
+let gridSize = parseInt(prompt("enter grid size."));
+if(gridSize > 80){
+  gridSize = 80;
+}
+squareDivGrid(gridSize);
 
 
 
